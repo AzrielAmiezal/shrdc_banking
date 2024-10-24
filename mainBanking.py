6527b929ee2b6d0334b98a1ba766fe77d3dcd19d
Azriel Amiezal <67147873+AzrielAmiezal@users.noreply.github.com>
2024-10-24 15:06:23 +0800

    Add files via upload

---
+++ mainBanking.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(page_title="Banking Analysis", layout="wide")
+
+# Title for the app
+st.title("Banking Data Analysis")
+
+# Add some content for the main page
+st.write("This is the main page of the Banking Data Analysis app.")
+
+# You can include an introductory message or overview of the dataset here.
+st.markdown("### Use the sidebar to navigate between pages")
